7832487c376e377930e73ce7023d56a0f2836b0e
Conor Shipsey <cpshipsey@gmail.com>
2023-11-28 16:26:45 +0000

    Adding constructor to ColourTable class

--- src/main/java/com/conorshipsey/colourtableca3/ColourTable.java
+++ src/main/java/com/conorshipsey/colourtableca3/ColourTable.java
@@ -1,4 +1,23 @@
 package com.conorshipsey.colourtableca3;
 
 public class ColourTable {
+    private final int numPalettes;
+
+    public ColourTable(int numPalettes) {
+        // if num palettes is a power of 2 & greater than 1 & less than 1025
+        if (numPalettes > 1 && numPalettes < 1025 && (isPowerOfTwo(numPalettes))) {
+            this.numPalettes = numPalettes;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid palette size");
+        }
+    }
+    public static boolean isPowerOfTwo(int num) {
+        if (num <= 0) {
+            return false;  // 0 and negative numbers are not powers of two
+        }
+        // Check if the logarithm to the base 2 is an integer
+        double logBase2 = Math.log(num) / Math.log(2);
+        return Math.floor(logBase2) == logBase2;
+    }
 }
