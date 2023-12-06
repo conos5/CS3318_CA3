35cf38b37ce7c2be07d2dff9166ef6469b8c544e
Conor Shipsey <cpshipsey@gmail.com>
2023-12-06 13:54:08 +0000

    Broke testAdd() down into smaller tests, as per TDD convention. This way it is easier to identify exactly which test breaks the code.

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -31,28 +31,40 @@ public class ColourTableTest {
     }
 
     @Test
-    public void testAdd() {
+    public void testAddValidColour() {
         // Testing a method add that enables a developer to add a 24-bit RGB
         // colour to the colour table
-        ColourTable ct = new ColourTable(4);
-         // Black in hexadecimal = 0x000000 ->
+        // Black in hexadecimal = 0x000000 ->
         // equivalent to 0b00000000_00000000_00000000 (24 bits)
+        // We should then test that the colour was added to the
+        // colour table with the isPresent() method
+        ColourTable ct = new ColourTable(4);
         ct.add("000000");
-        // We should then test that the colour was added to the colour table
         assertTrue(ct.isPresent("000000"));
-        // We should be thrown an exception if we try to add a colour that is
-        assertThrows(IllegalArgumentException.class, () -> ct.add("000000"));
-        // We should then test that the colour was added to the colour table
-        // This will require a method to get the colour table
+    }
 
-        // Tests to ensure that the colour table throws an exception
-        //   when full and trying to add a colour
+    @Test
+    public void testAddDuplicateColour() {
+        ColourTable ct = new ColourTable(4);
+        ct.add("000000");
+        assertThrows(IllegalArgumentException.class, () -> ct.add("000000"));
+    }
+
+    @Test
+    public void testAddInvalidColour() {
+        // Testing adding an invalid hexadecimal colour
+        ColourTable ct = new ColourTable(4);
+        assertThrows(IllegalArgumentException.class, () -> ct.add("00000X"));
+    }
+
+    @Test
+    public void testAddColourToFullTable() {
+        ColourTable ct = new ColourTable(4);
         ct.add("FF0000");
         ct.add("00FF00");
         ct.add("0000FF");
-        assertThrows(IllegalArgumentException.class, () -> ct.add("FFFFFF"));// adding new colour
-            // to full colour table should throw an exception
+        ct.add("000000");
+        // to add to full colour table should throw an exception
+        assertThrows(IllegalArgumentException.class, () -> ct.add("FFFFFF"));
     }
-
-
 }
